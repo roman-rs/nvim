967fd2bc9e18af367303469662eabf6722771f7a
Roman Semenov <r.a.semenov@gmail.com>
2025-07-27 18:54:54 -0700

    fix: added transparency

--- init.lua
+++ init.lua
@@ -10,3 +10,6 @@ require("config.keymaps")
 
 require("config.extra")
 
+-- transparency
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
