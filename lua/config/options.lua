2dfba617e5f375e025d5700f75fe2464580f322d
Roman Semenov <r.semenov@f5.com>
2026-01-06 11:42:50 -0800

    fix: clipboard sharing over ssh using OSC 52

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -41,3 +41,15 @@ vim.opt.winborder = "rounded"
 
 
 vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+}
+
