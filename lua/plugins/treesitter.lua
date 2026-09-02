3df56f713437f6abd149672f8eeabc797c35185a
Roman Semenov <r.semenov@f5.com>
2026-09-02 13:02:39 -0700

    fixed treesitter and added markdown viewer plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,34 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = 'master',
+  branch = "main",
   lazy = false,
-  build = ":TSUpdate"
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter").install({
+      "markdown",
+      "markdown_inline",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "bash",
+      "c",
+      "cpp",
+      "json",
+      "yaml",
+      "perl",
+      "php",
+    })
+
+    -- The `main` branch no longer auto-enables highlighting; do it per-buffer.
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        local buf = args.buf
+        local ok, parser = pcall(vim.treesitter.get_parser, buf, nil, { error = false })
+        if ok and parser then
+          vim.treesitter.start(buf, parser:lang())
+        end
+      end,
+    })
+  end,
 }
